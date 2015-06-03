5ae26ba4e0de781b21c0482015d556f00d8b7bd4
free1 <747549945@qq.com>
2015-06-03 11:29:30 +0800

    init

---
+++ lib/omniauth/open_wechat.rb
@@ -0,0 +1,7 @@
+require "omniauth/open_wechat/version"
+
+module Omniauth
+  module OpenWechat
+    # Your code goes here...
+  end
+end
---
+++ lib/omniauth/open_wechat/version.rb
@@ -0,0 +1,5 @@
+module Omniauth
+  module OpenWechat
+    VERSION = "0.1.0"
+  end
+end
